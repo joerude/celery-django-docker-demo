bcf55d781e3a07e8e4763c001ed6ea99f938bf32
ma4akk <ma4akk@gmail.com>
2022-05-13 02:13:25 +0300

    first init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Celery Django Funny Cats Downloader
+
+## Demo project to demonstrate how Celery + Redis + Django work
+
+Run commands:
+
+```
+git clone https://github.com/joerude/celery-django-docker-demo
+cd celery-django-docker-demo
+docker-compose up --build
+```
+
+Open localhost(http://127.0.0.1:8000/)
---
+++ webapp/src/main/tasks.py
@@ -0,0 +1,19 @@
+import requests
+import uuid
+from celery import shared_task
+
+from django.conf import settings
+
+CAT_URL = 'https://cataas.com/cat'
+
+
+@shared_task
+def download_a_cat():
+    resp = requests.get(CAT_URL)
+    file_ext = resp.headers.get('Content-Type').split('/')[1]
+    file_name = settings.BASE_DIR / 'cats' / (str(uuid.uuid4()) + "." + file_ext)
+
+    with open(file_name, 'wb') as f:
+        for chunk in resp.iter_content(chunk_size=128):
+            f.write(chunk)
+    return True
